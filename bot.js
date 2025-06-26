2f797839c734206ff513bc8d0a23ba57c387758e
john <williamlemus887@gmail.com>
2025-06-26 19:35:57 -0400

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,26 @@
+const mineflayer = require('mineflayer');
+
+const bot = mineflayer.createBot({
+  host: 'PLUMSMP.aternos.me',
+  port:  12862,
+  username: 'AFKbot'
+});
+
+bot.on('chat', (username, message) => {
+  if (username === bot.username) return;
+  if (message === 'hi') {
+    bot.chat(`Hello, ${username}!`);
+  }
+});
+
+bot.on('spawn', () => {
+  console.log('Bot has joined the server!');
+});
+
+bot.on('end', () => {
+  console.log('Bot has disconnected');
+});
+
+bot.on('error', err => {
+  console.log('Bot error:', err);
+});
