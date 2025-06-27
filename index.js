55c85dcaac1942469f52af413f4e1f4ab2f48a8c
john <williamlemus887@gmail.com>
2025-06-27 16:05:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Bot is running.'));
+app.listen(3000, () => console.log('Health endpoint on port 3000'));
+
 require('./bot.js');
