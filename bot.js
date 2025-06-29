713f984ccbe5ea3946ecb5eb88e73a4f1fe8d78a
john <williamlemus887@gmail.com>
2025-06-29 19:30:07 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,56 +1,24 @@
 const mineflayer = require('mineflayer');
-const { pathfinder, Movements, goals } = require('mineflayer-pathfinder');
-const { Vec3 } = require('vec3');
 
-const bot = mineflayer.createBot({
-  host: 'PLUMSMP.aternos.me',
-  port: 12862,
-  username: 'AFKbot'
-});
+function createBot() {
+  const bot = mineflayer.createBot({
+    host: 'PLUMSMP.aternos.me',
+    port: 12862,
+    username: 'AFKbot'
+  });
 
-const directions = [
-  new Vec3(0, 0, -4),
-  new Vec3(4, 0, 0),
-  new Vec3(0, 0, 4),
-  new Vec3(-4, 0, 0)
-];
+  bot.on('spawn', () => {
+    console.log('Bot has joined the server!');
+  });
 
-let current = 0;
+  bot.on('end', () => {
+    console.log('Bot disconnected, reconnecting in 10s...');
+    setTimeout(createBot, 10000);
+  });
 
-bot.loadPlugin(pathfinder);
+  bot.on('error', err => {
+    console.log('Bot error:', err);
+  });
+}
 
-bot.on('spawn', () => {
-  console.log('Bot has joined the server!');
-  const defaultMove = new Movements(bot);
-  bot.pathfinder.setMovements(defaultMove);
-
-  setInterval(() => {
-    bot.chat('/setidletimeout 3000');
-    console.log('Sent /setidletimeout 3000');
-  }, 60000);
-
-  setInterval(() => {
-    const pos = bot.entity.position.offset(
-      directions[current].x,
-      directions[current].y,
-      directions[current].z
-    );
-    bot.pathfinder.setGoal(new goals.GoalBlock(pos.x, pos.y, pos.z));
-    current = (current + 1) % directions.length;
-  }, 10000);
-});
-
-bot.on('chat', (username, message) => {
-  if (username === bot.username) return;
-  if (message === 'hi') {
-    bot.chat(`Hello, ${username}!`);
-  }
-});
-
-bot.on('end', () => {
-  console.log('Bot has disconnected');
-});
-
-bot.on('error', err => {
-  console.log('Bot error:', err);
-});
+createBot();
