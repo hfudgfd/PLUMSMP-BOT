d53b498115ab5e1268ebf01eeeecf0da4aef3832
john <williamlemus887@gmail.com>
2025-06-29 12:36:58 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,6 @@
 const mineflayer = require('mineflayer');
+const { pathfinder, Movements, goals } = require('mineflayer-pathfinder');
+const { Vec3 } = require('vec3');
 
 const bot = mineflayer.createBot({
   host: 'PLUMSMP.aternos.me',
@@ -6,12 +8,36 @@ const bot = mineflayer.createBot({
   username: 'AFKbot'
 });
 
+const directions = [
+  new Vec3(0, 0, -4),
+  new Vec3(4, 0, 0),
+  new Vec3(0, 0, 4),
+  new Vec3(-4, 0, 0)
+];
+
+let current = 0;
+
+bot.loadPlugin(pathfinder);
+
 bot.on('spawn', () => {
   console.log('Bot has joined the server!');
+  const defaultMove = new Movements(bot);
+  bot.pathfinder.setMovements(defaultMove);
+
   setInterval(() => {
     bot.chat('/setidletimeout 3000');
     console.log('Sent /setidletimeout 3000');
   }, 60000);
+
+  setInterval(() => {
+    const pos = bot.entity.position.offset(
+      directions[current].x,
+      directions[current].y,
+      directions[current].z
+    );
+    bot.pathfinder.setGoal(new goals.GoalBlock(pos.x, pos.y, pos.z));
+    current = (current + 1) % directions.length;
+  }, 10000);
 });
 
 bot.on('chat', (username, message) => {
